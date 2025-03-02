e122fb09d7f4ae648c0d4534bae2be276f37b771
Hang Xie <7977860+hangxie@users.noreply.github.com>
2025-03-02 22:45:41 +0000

    tweak start.sh for ssh-agent

--- start.sh
+++ start.sh
@@ -4,12 +4,15 @@ set -euo pipefail
 
 VERSION=${1:-latest}
 
-docker run -d --rm --name go-code-server \
+# you want to start a tmux/screen session for ssh agent session
+# eval `ssh-agent`
+# ssh-add
+docker run -it --rm --name go-code-server \
     -p 127.0.0.1:8080:8080 \
     -v "$HOME/.config:/home/coder/.config" \
     -v "$HOME/dev:/home/coder/project" \
-    -v "$SSH_AUTH_SOCK:$SSH_AUTH_SOCK" \
+    -v "$SSH_AUTH_SOCK:/home/coder/.ssh/$(basename $SSH_AUTH_SOCK)" \
     -u "$(id -u):$(id -g)" \
     -e "DOCKER_USER=$USER" \
-    -e "SSH_AUTH_SOCK=$SSH_AUTH_SOCK" \
+    -e "SSH_AUTH_SOCK=/home/coder/.ssh/$(basename $SSH_AUTH_SOCK)" \
     hangxie/go-code-server:${VERSION}
